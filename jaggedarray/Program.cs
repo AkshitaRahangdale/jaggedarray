9946f4e56cc2ece5db515e6822dd88e33473b7f0
AkshitaRahangdale <akshitarahangdale@1720gmail.com>
2022-10-19 11:51:43 +0530

    Add project files.

---
+++ jaggedarray/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jaggedarray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] jarray = new int[3][];
+
+            jarray[0] = new int[] { 1,2,3,4,5};
+            jarray[1] = new int[] { 10,20,30};
+            jarray[2] = new int[] { 100,200 };
+
+            for(int i = 0; i < jarray.Length;i++)
+            {
+                for(int j = 0; j < jarray[i].Length;j++)
+                {
+                    Console.Write(" " + jarray[i][j]);
+                }
+                Console.WriteLine();
+            }
+
+
+        }
+    }
+}
